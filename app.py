e499570fe7ae8c5e9a43a5380ffe104e24ddfd5a
CrappyDill <mddillon47@gmail.com>
2021-08-18 13:24:35 -0700

    add pet form working

--- app.py
+++ app.py
@@ -22,16 +22,20 @@ def show_pets():
     return render_template("/show_pets.html", pets=pets)
 
 
-@app.route("/add")
-def show_pets():
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
     form = AddPetForm()
 
     if form.validate_on_submit():
-        # name = form.name.data
-        # species = form.species.data
-        # photo_url = form.photo_url.data
-        # age = form.age.data
-        # notes = form.notes.data
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        newPet = Pet(name=name, species=species,
+                     photo_url=photo_url, age=age, notes=notes)
+        db.session.add(newPet)
+        db.session.commit()
         return redirect("/")
     else:
-        return render_template("/.html")
+        return render_template("/add_pet_form.html", form=form)
--- forms.py
+++ forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import InputRequired
 
 class AddPetForm(FlaskForm):
 
-    name = StringField("Pet name", validators=InputRequired())
-    species = StringField("Species", validators=InputRequired())
-    photo_url = StringField("Photo")
+    name = StringField("Pet name", validators=[InputRequired()])
+    species = StringField("Species", validators=[InputRequired()])
+    photo_url = StringField("Photo URL")
     age = IntegerField("Age")
     notes = StringField("Notes")
