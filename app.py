551a7fc965f21773f6ad60679a01acfbe527a57a
CrappyDill <mddillon47@gmail.com>
2021-08-17 18:03:05 -0700

    beginning of AddPetForm

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db,  connect_db, Pet
-
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -20,3 +20,18 @@ db.create_all()
 def show_pets():
     pets = Pet.query.all()
     return render_template("/show_pets.html", pets=pets)
+
+
+@app.route("/add")
+def show_pets():
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        # name = form.name.data
+        # species = form.species.data
+        # photo_url = form.photo_url.data
+        # age = form.age.data
+        # notes = form.notes.data
+        return redirect("/")
+    else:
+        return render_template("/.html")
---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
+from wtforms.validators import InputRequired
+
+
+class AddPetForm(FlaskForm):
+
+    name = StringField("Pet name", validators=InputRequired())
+    species = StringField("Species", validators=InputRequired())
+    photo_url = StringField("Photo")
+    age = IntegerField("Age")
+    notes = StringField("Notes")
